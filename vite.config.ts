01a81312ac3629d9008a6b4d68465ec22d50bf36
ANISHKARTHIC <anishkarthicvs@gmail.com>
2026-09-18 22:09:46 +0530

    chore: crop and apply Brownie Brothers logo everywhere

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      includeAssets: ['favicon.png', 'apple-touch-icon.png', 'logo.png'],
       manifest: {
         name: 'Brownie Brothers',
         short_name: 'Brownie Brothers',
